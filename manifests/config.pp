ec1fc733971941b51489bc2f7fb4ebeac5094d72
Jake Yip <jake.yip@unimelb.edu.au>
2020-10-07 12:58:43 +1100

    Remove sysctl dependencies
    We have a conflicting module duritong/sysctl, so this will not install
    cleanly using puppet-librarian
    
    Anyway, the default user.max_user_namespaces on RHEL8 is 1540215, so
    this is practically useless.
    
    Change-Id: If73b4041a107e576b97e0f9b7c65c0383468ca41

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,8 +5,4 @@
 # @example
 #   include podman::config
 class podman::config {
-  sysctl { 'user.max_user_namespaces':
-    ensure => present,
-    value  => '3154', # This is the default in RHEL8.1beta
-  }
 }
