6c20da499dabca80fe3218c636e05cb881784062
Jake Yip <jake.yip@unimelb.edu.au>
2020-11-09 14:15:57 +1100

    Add run command
    Change-Id: Id10be14ee375acc8acea3e60b07c517bccf72b3b

--- manifests/run.pp
+++ manifests/run.pp
@@ -57,6 +57,7 @@ define podman::run (
       {
         'image'                 => $image,
         'container_tag'         => $container_tag,
+        'command'               => $command,
         'volumes'               => $volumes,
         'container_extra_flags' => $container_extra_flags,
         'podman_extra_args'     => $podman_extra_args,
