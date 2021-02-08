efe619be3b8094dd5469f7e09cbd5e4961fd1d0c
Shi Yan <shi.yan@unimelb.edu.au>
2021-02-08 12:08:56 +1100

    Add ubuntu podman install
    After 20.10, podman should be in official repository.
    
    Change-Id: I41876550fb34bad1d13fa6a273656c94830bcf9e

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,6 +5,20 @@
 # @example
 #   include podman::install
 class podman::install {
+
+  $version = $facts['os']['release']['full']
+  if (versioncmp($version, '20.10') < 0) {
+    case $::osfamily {
+      'Debian': {
+        apt::source { 'podman':
+          location => "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${version}/",
+          repos    => '/',
+          release  => '',
+        } -> Package['podman']
+      }
+    }
+  }
+
   package { 'slirp4netns':
     ensure => present,
   }
