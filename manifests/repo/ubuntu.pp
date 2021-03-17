6db2c9857a53f642e3efa55f6ffe137ce6332960
Kieran Spear <kispear@gmail.com>
2021-03-17 19:21:36 +1100

    Add repo key
    Change-Id: Ia6c8aae7046c673b6e608a22716fd7539e919960

--- manifests/repo/ubuntu.pp
+++ manifests/repo/ubuntu.pp
@@ -3,10 +3,16 @@ class podman::repo::ubuntu {
   $version = $::facts['os']['release']['full']
 
   if (versioncmp($version, '20.10') < 0) {
-    apt::source { 'podman':
+    include apt
+    apt::key { 'kubic':
+      id     => '2472D6D0D2F66AF87ABA8DA34D64390375060AA4',
+      source => "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${version}/Release.key",
+    }
+    -> apt::source { 'podman':
       location => "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${version}/",
       repos    => '/',
       release  => '',
-    } -> Package['podman']
+    }
+    -> Package['podman']
   }
 }
